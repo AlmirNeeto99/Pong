705b1ca3987ed1373d8e23db202768ede1901617
Almir Neto <almirneto338@gmail.com>
2021-06-05 15:37:37 -0300

    implementing  paddle constructor and draw functions

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -1,1 +1,22 @@
 #include "headers/Paddle.hpp"
+#include "GL/gl.h"
+
+Paddle::Paddle(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Paddle::~Paddle() {}
+
+void Paddle::update() {}
+
+void Paddle::draw()
+{
+    glBegin(GL_QUADS);
+    glVertex2i(this->x, this->y);
+    glVertex2i(this->x + this->width, this->y);
+    glVertex2i(this->x + this->width, this->y + this->height);
+    glVertex2i(this->x, this->y + this->height);
+    glEnd();
+}
