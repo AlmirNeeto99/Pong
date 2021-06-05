353f9d8b913b11fa83b576f1dd11a7e57f7bd42a
Almir Neto <almirneto338@gmail.com>
2021-06-05 15:37:07 -0300

    defining paddle attributes and methods

--- src/headers/Paddle.hpp
+++ src/headers/Paddle.hpp
@@ -3,8 +3,16 @@
 
 class Paddle
 {
-    Paddle();
+public:
+    int x, y;
+    Paddle(int x, int y);
     ~Paddle();
+    void update();
+    void draw();
+
+private:
+    const int width = 15;
+    const int height = 150;
 };
 
 #endif
