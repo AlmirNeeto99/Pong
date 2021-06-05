cb2265027d3f4fbc475f3b8fc3e80d7003e0c871
Almir Neto <almirneto338@gmail.com>
2021-06-05 17:48:47 -0300

    drawing ball

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,21 +1,35 @@
 #include "headers/Ball.hpp"
 #include "GL/gl.h"
+#include <cmath>
 
-Ball::Ball() {}
-
-Ball::Ball(int x, int y) {}
+Ball::Ball(int x, int y, int radius)
+{
+    this->x = x;
+    this->y = y;
+    this->radius = radius;
+}
 
 Ball::~Ball() {}
 
 void Ball::move(int x, int y) {}
 
-void Ball::update() {}
+void Ball::update()
+{
+    this->x += 5;
+    this->y += 5;
+    if (this->x >= 1000 - this->radius)
+        this->x = 0 + this->radius;
+
+    if (this->y >= 600 - this->radius)
+        this->y = 0 + this->radius;
+}
 
 void Ball::draw()
 {
-    glBegin(GL_TRIANGLES);
-    glVertex2f(500, 150);
-    glVertex2f(250, 500);
-    glVertex2f(750, 500);
+    glColor3f(1, (float)51 / 255, (float)51 / 255);
+    glBegin(GL_POLYGON);
+    for (int deg = 0; deg <= 360; deg += 1)
+        glVertex2i(this->x + (this->radius * cos(deg)), this->y + (this->radius * sin(deg)));
+
     glEnd();
 }
