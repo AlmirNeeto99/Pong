68081df8e931b9e8f06c3e542137c7fbcb990b1e
Almir Neto <almirneto338@gmail.com>
2021-06-06 16:43:42 -0300

    changing the way events are listened

--- src/KeyListener.cpp
+++ src/KeyListener.cpp
@@ -2,13 +2,16 @@
 #include "headers/events/KeyEvent.hpp"
 #include "GLFW/glfw3.h"
 
-int KeyListener::keyPressed(char k)
+int KeyListener::keyPressed(Key key)
 {
     int action = KeyEvent::getAction();
+    /* If the key was PRESSED or HELD */
     if (action == GLFW_PRESS || action == GLFW_REPEAT)
     {
-        return KeyEvent::getKey() == k;
+        /* I used ternary here to be sure the return will be 0 or 1 */
+        return KeyEvent::getKey() == key ? GLFW_TRUE : GLFW_FALSE;
     }
+    return GLFW_FALSE;
 }
 int KeyListener::keyReleased()
 {
